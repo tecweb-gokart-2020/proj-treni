55eb08012d1f21283c25b5b0da827dba89b5d876
RedSut <51701714+RedSut@users.noreply.github.com>
2020-12-30 15:01:07 +0100

    Aggiunti controlli su $cart_id

--- src/includes/functions/carrello.php
+++ src/includes/functions/carrello.php
@@ -3,31 +3,43 @@ require_once "../resources.php";
 use DB\DbAccess;
 
 function getSession($cart_id){
-    $dbAccess = new DBAccess();
-    $dbAccess->opendbconnection();
-    $query = "SELECT sessionID FROM carrello WHERE cartID = \"$cart_id\"";
-    $queryResult = mysqli_query($dbAccess->connection, $query);    
-    $dbAccess->closedbconnection();
-    return $queryResult;
+    $reg_expr = "/^\d+$/";
+    if(preg_match($reg_expr,$cart_id)!=0){
+        $dbAccess = new DBAccess();
+        $dbAccess->opendbconnection();
+        $query = "SELECT sessionID FROM carrello WHERE cartID = \"$cart_id\"";
+        $queryResult = mysqli_query($dbAccess->connection, $query);    
+        $dbAccess->closedbconnection();
+        return $queryResult;
+    }else{
+        error_log("Invalid cartID! Must be a number");
+        return false;
+    }
 }
 
 function getProdotti($cart_id){
-    $dbAccess = new DBAccess();
-    $dbAccess->opendbconnection();
-    $query = "SELECT codArticolo, quantita FROM contenuto_carrello WHERE cartID = \"$cart_id\"";
-    $queryResult = mysqli_query($dbAccess->connection, $query);
-    $listaProdotti = array();
-    if(mysqli_num_rows($queryResult)!=0){
-        while($riga = mysqli_fetch_assoc($queryResult)){
-            $singoloProdotto = array(
-                "IDArticolo", $riga["codArticolo"],
-                "Qta", $riga["quantita"]
-            );    
-            array_push($listaProdotti,$singoloProdotto);
+    $reg_expr = "/^\d+$/";
+    if(preg_match($reg_expr,$cart_id)!=0){
+        $dbAccess = new DBAccess();
+        $dbAccess->opendbconnection();
+        $query = "SELECT codArticolo, quantita FROM contenuto_carrello WHERE cartID = \"$cart_id\"";
+        $queryResult = mysqli_query($dbAccess->connection, $query);
+        $listaProdotti = array();
+        if(mysqli_num_rows($queryResult)!=0){
+            while($riga = mysqli_fetch_assoc($queryResult)){
+                $singoloProdotto = array(
+                    "IDArticolo", $riga["codArticolo"],
+                    "Qta", $riga["quantita"]
+                );    
+                array_push($listaProdotti,$singoloProdotto);
+            }
         }
+        $dbAccess->closedbconnection();
+        return $listaProdotti;
+    }else{
+        error_log("Invalid cartID! Must be a number");
+        return false;
     }
-    $dbAccess->closedbconnection();
-    return $listaProdotti;
 }
 
 ?>
