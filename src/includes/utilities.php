c4b1140f5a68e0d652653d820271d2a31ce14525
RedSut <51701714+RedSut@users.noreply.github.com>
2020-12-31 10:54:11 +0100

    Aggiunta funzione isValidShipping

--- src/includes/utilities.php
+++ src/includes/utilities.php
@@ -1,6 +1,6 @@
 <?php
 namespace UTILITIES;
-require_once "../resources.php";
+//require_once "resources.php";
 
 function isValidCarrello($cart_id){
     $reg_expr = "/^\d+$/";
@@ -8,6 +8,16 @@ function isValidCarrello($cart_id){
         return true;
     }else{
         error_log("Invalid cartID! Must be a number");
+        return false;
+    }
+}
+
+function isValidShipping($shipping_id){
+    $reg_expr = "/^\d+$/";
+    if(preg_match($reg_expr,$shipping_id)!=0){
+        return true;
+    }else{
+        error_log("Invalid shippingID! Must be a number");
         return false;
     }
 }
