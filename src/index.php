1e66aa623a17f74d0416f49d8561328915a6724b
Luca Zaninotto <lucazanny@gmail.com>
2021-01-01 11:10:22 +0100

    init dell'index per iniziare ad avere struttura

---
+++ src/index.php
@@ -0,0 +1,13 @@
+<?php
+// unico file da includere con tutto il backend
+require_once __DIR__ . DIRECTORY_SEPARATOR . "includes/resources.php";
+
+// variabili riferite alla singola pagina
+$page_title = "Homepage";
+$page_desc = "Descrizione della homepage";
+
+// Esempio di inclusione delle viste header, corpo di index e footer
+require_once __DIR__ . DIRECTORY_SEPARATOR . "views/template/header.php";
+require_once __DIR__ . DIRECTORY_SEPARATOR . "views/template/showIndex.p;hp";
+require_once __DIR__ . DIRECTORY_SEPARATOR . "views/template/footer.php";
+?>
