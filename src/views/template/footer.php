0b6d2d8ffeb8f5001d6ec071c291ccf8b9ba0e83
BitStuffer <76245709+BitStuffer@users.noreply.github.com>
2021-01-03 10:18:03 +0100

    Update footer.php
    Sistemata indentazione

--- src/views/template/footer.php
+++ src/views/template/footer.php
@@ -1,20 +1,20 @@
 <footer>
-  <address><!-- informazioni di contatto e sul negozio -->
-			<div id="indirizzi">
-				<p><strong>Vieni a trovarci!</strong> <br/> Esposizione <br/> via Pordenone, 6 - 30026 PORTOGRUARO (VE)</p>
-				<p>Sede legale <br/> via della Torba, 10 - 33080 PORCIA (PN)</p>
-			</div>
-			<!-- tutti quei <br/> sono rimpiazzabili da line-height(?) in CSS -->
-			<div id="contatti">
-				<p><strong>Contattaci!</strong> <br/> Email <br/> <a href="mailto:info@trenogheno.it">info@trenogheno.it</a> <br/><br/>
-				Telefono <br/> +39 348 443 19 33<br/><br/>
-				Fax <br/> +39 0434 572 865</p>
-			</div>
+ 	<address><!-- informazioni di contatto e sul negozio -->
+		<div id="indirizzi">
+			<p><strong>Vieni a trovarci!</strong> <br/> Esposizione <br/> via Pordenone, 6 - 30026 PORTOGRUARO (VE)</p>
+			<p>Sede legale <br/> via della Torba, 10 - 33080 PORCIA (PN)</p>
+		</div>
+		<!-- tutti quei <br/> sono rimpiazzabili da line-height(?) in CSS -->
+		<div id="contatti">
+			<p><strong>Contattaci!</strong> <br/> Email <br/> <a href="mailto:info@trenogheno.it">info@trenogheno.it</a> <br/><br/>
+			Telefono <br/> +39 348 443 19 33<br/><br/>
+			Fax <br/> +39 0434 572 865</p>
+		</div>
 
-			<div id="quellaRigaSotto">
-				<p>Trenogheno S.A.S. di Rigon Angelo & C. - Vendita per corrispondenza articoli per fermodellismo - P.I. e Reg.Imprese 01848890933</p>
-			</div>
-		</address>
+		<div id="quellaRigaSotto">
+			<p>Trenogheno S.A.S. di Rigon Angelo & C. - Vendita per corrispondenza articoli per fermodellismo - P.I. e Reg.Imprese 01848890933</p>
+		</div>
+	</address>
 </footer>
 </body>
 </html>
