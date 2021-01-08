9fae8df6eb4a30f9d1cbf456f6c4a0739fd48440
Luca Zaninotto <lucazanny@gmail.com>
2021-01-08 12:37:43 +0100

    final

--- src/views/info.php
+++ src/views/info.php
@@ -4,32 +4,29 @@ use DB\DBAccess;
 use function ACCOUNT\getEmailOfAccount;
 
 session_start();
-if(isset($_SESSION["username"]) or true) {
+if(isset($_SESSION["username"])) {
     /* Se l'utente è autenticato mostrerà la pagina giusta, farà
      * invece un redirect alla home se non lo è (caso in cui l'utente
      * richiede la pagina direttamente da url, invece che dalla home
      * dopo il login) */
 
-    // $area_personale_link = "class=\"header_current_link\"";
-    // $notizie_link = "href=\"notizie.php\"";
-    // $prodotti_link = "href=\"prodotti.php\"";
-    // $servizi_link = "href=\"servizi.php\"";
-    // $contatti_link = "href=\"contatti.php\"";
-    // include "template/header.php";
+    $area_personale_link = "class=\"header_current_link\"";
+    $notizie_link = "href=\"notizie.php\"";
+    $prodotti_link = "href=\"prodotti.php\"";
+    $servizi_link = "href=\"servizi.php\"";
+    $contatti_link = "href=\"contatti.php\"";
+    include "template/header.php";
 
-    // $info_personali_link = "class=\"active_link\"";
-    // $ordini_link = "href=\"ordini.php\"";
-    // $indirizzi_link = "href=\"indirizzi.php\"";
-    // include "template/ap_navbar.php";
+    $info_personali_link = "class=\"active_link\"";
+    $ordini_link = "href=\"ordini.php\"";
+    $indirizzi_link = "href=\"indirizzi.php\"";
+    include "template/ap_navbar.php";
 
-    // /* Contenuto reale della pagina */
-    // $_SESSION["username"] = "user";
-    // $user = $_SESSION["username"];
-    $email = getEmailOfAccount("user") ? getEmailOfAccount("user") : "Madonna arcana";
-    // $real_email = $email ? $email : "email non trovata";
-    // echo "<ul>" . HTML_EOL . "<li>username: $user</li>" . HTML_EOL . "<li>email: $real_email</li>". HTML_EOL . "</ul>";
-    echo $email;
-    // include "template/footer.php";
+    /* Contenuto reale della pagina */
+    $user = $_SESSION["username"];
+    $email = getEmailOfAccount("user");
+    echo "<ul>" . PHP_EOL . "<li>username: $user</li>" . PHP_EOL . "<li>email: $email</li>". PHP_EOL . "</ul>";
+    include "template/footer.php";
 }
 else {
     echo "username risulta non settato :(";
