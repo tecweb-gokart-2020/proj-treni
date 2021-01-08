ad9c24f65aa19e39ae7ac5ee50ae708c7b9079c7
Luca Zaninotto <lucazanny@gmail.com>
2021-01-08 12:13:52 +0100

    ora.

--- src/includes/class/db.class.php
+++ src/includes/class/db.class.php
@@ -4,47 +4,19 @@ require_once __DIR__ . DIRECTORY_SEPARATOR . '../resources.php';
 use mysqli;
 
 class DBAccess {
-    // private const HOST_DB = "localhost";
-    // private const USERNAME = $_SERVER["LOGNAME"];
-    private $pw_file;
-    // private const DB_NAME = $_SERVER["LOGNAME"];
-    // private const PORT = 3306;
+    private const HOST_DB = "localhost";
+    private const USERNAME = $_SERVER["LOGNAME"];
+    /* Ci rinuncio, impostate come password "password"...*/
+    private const PASSWORD = "password";
+    private const DB_NAME = $_SERVER["LOGNAME"];
+    private const PORT = 3306;
 
-    private function __get($constName){
-        $val = null;
-        switch($constName) {
-        case 'HOST_DB':
-            $val = 'localhost';
-            break;
-        case 'USERNAME':
-            $val = $_SERVER["LOGNAME"];
-            break;
-        case 'DB_NAME':
-            $val = $_SERVER["LOGNAME"];
-            break;
-        case 'PORT':
-            $val = 3306;
-            break;
-        }
-        return $val;
-    }
-    
     private $connection;
 
     public function initDbConnection() {
-        // a questo punto funziona solo su le macchine di ateneo ma
-        // dovrebbe funzionare
-        $home = isset($_SERVER["HOME"]) ? $_SERVER["HOME"] : "/home" . $_SERVER['CONTEXT_PREFIX'];
-        echo $home;
-        $fname = $home . DIRECTORY_SEPARATOR . "pwd_db_2020-21.txt";
-        $pw_file = fopen($fname, 'r');
-        $pw_file_size = filesize($fname);
-
-        $password = str_replace("\n", "", fread($pw_file, $pw_file_size));
-        
         $this->connection = mysqli_connect($this->HOST_DB,
                                            $this->USERNAME,
-                                           $password,
+                                           $this->PASSWORD,
                                            $this->DB_NAME,
                                            $this->PORT);
         if(!$this->connection){
@@ -55,7 +27,6 @@ class DBAccess {
     }
 
     public function openDbConnection() {
-        // per evitare memory leak sulla stessa istanza di connessione
         if(!$this->connection) {
             return $this->initDbConnection();
         } else {
