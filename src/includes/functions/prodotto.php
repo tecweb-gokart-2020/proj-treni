cabde4b8bbcddc82d1a698743bc9d1b74089a258
RedSut <51701714+RedSut@users.noreply.github.com>
2021-01-14 17:46:45 +0100

    Update prodotto.php
    Modificata query ricerca

--- src/includes/functions/prodotto.php
+++ src/includes/functions/prodotto.php
@@ -85,13 +85,13 @@ function queryProdotti($categoria, $marca ="Nessuna selezione", $disponibile ="d
 function searchProdotti($stringa){
     $dbAccess = new DBAccess();
     $connection = $dbAccess->openDbConnection();
-    $query = "SELECT codArticolo FROM prodotti WHERE codArticolo LIKE \"$stringa\" OR descrizione LIKE \"$stringa\"
-            OR marca LIKE \"$stringa\"";
+    $query = "SELECT codArticolo FROM prodotto WHERE (codArticolo LIKE '%".$stringa."%' OR descrizione LIKE '%".$stringa."%' OR 
+    marca LIKE '%".$stringa."%' OR tipo LIKE '%".$stringa."%')";
     $queryResult = mysqli_query($connection, $query);
     $dbAccess->closeDbConnection();
     $prodotti = array();
     while($singoloProdotto = mysqli_fetch_row($queryResult)){
-        array_push($marche,$singoloProdotto[0]);
+        array_push($prodotti,$singoloProdotto[0]);
     };
     return $prodotti;
 }
