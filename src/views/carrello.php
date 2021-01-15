4db1dc4fc0104e76644a26da2b329e630ed91cfc
Luca Zaninotto <lucazanny@gmail.com>
2021-01-15 08:58:25 +0100

    struttura improved

--- src/views/carrello.php
+++ src/views/carrello.php
@@ -1,16 +1,20 @@
 <?php
 require_once __DIR__ . DIRECTORY_SEPARATOR . "../includes/resources.php";
 use function CARRELLO\getNewCarrello;
+use function CARRELLO\getProdottiFromCarrello;
+
 session_start();
 if(!isset($_SESSION["cartID"])) {
     $newcart = getNewCarrello();
     if($newcart) {
         $_SESSION["cartID"] = $newcart;
     } else {
+        /* Caso davvero estremo, non dovrebbe mai capitare */
         error_log("Qualcosa è andato storto... impossibile inizializzare un nuovo carrello");
         die();
     }
 }
+
 // cartID correttamente impostato
 $area_personale_link = "href=\"info.php\"";
 $notizie_link = "href=\"notizie.php\"";
@@ -19,7 +23,20 @@ $servizi_link = "href=\"servizi.php\"";
 $contatti_link = "href=\"contatti.php\"";
 include "template/header.php";
 
-echo "<p>l'id del tuo carrello è " . $_SESSION["cartID"] . "</p>";
+$content = getProdottiFromCarrello($_SESSION["cartID"]);
+if($content) {
+    echo "</h2>Il tuo carrello:</h2>" . PHP_EOL;
+    echo "<ul id=\"cart\">" . PHP_EOL;
+    foreach($content as $prodotto) {
+        echo "<li>" . PHP_EOL;
+        // printProdotto($prodotto);
+        echo "</li>" . PHP_EOL;
+    }
+    echo "</ul>";
+}
+else {
+    echo "</h2>Il tuo carrello è vuoto al momento.</h2>" . PHP_EOL;
+}
 
 include "template/footer.php";
 ?>
