aa7fc4bcb7f733507c5fc9da5b496da3558355d8
Luca Zaninotto <lucazanny@gmail.com>
2021-01-15 09:07:55 +0100

    prova con l'header dei link semi-assoluti

--- src/views/template/header.php
+++ src/views/template/header.php
@@ -11,7 +11,7 @@ echo '<!DOCTYPE html>
     <body>
 	<header>
 	    <!-- Logo -->
-	    <a href="/" id="banner"><h1>TrenoGheno</h1></a>
+	    <a href="/index.php" id="banner"><h1>TrenoGheno</h1></a>
 	    <!-- Ricerca -->
 	    <form id="ricercaHeader" action="" method="get">
 		<input type="search" id="searchQuery" name="searchQuery" placeholder="Cosa cerchi?" maxlength="40"/>
@@ -22,27 +22,27 @@ echo '<!DOCTYPE html>
 		}
 	    echo '</form>
 	    <!-- Login -->
-	    <a href="heh">Login</a>
+	    <a href="/views/login.php">Login</a>
 	    <!-- Carrello -->
-	    <a href="huh">Carrello</a>
+	    <a href="/views/carrello.php">Carrello</a>
 	    <!-- Menù -->
 	    <nav>
 		<ul>
 		    <li>
 			<!-- Test -->
-			<a <?=$area_personale_link;?>>Area personale</a>
+			<a "/views/info.php">Area personale</a>
 		    </li>
 		    <li>
-			<a <?=$notizie_link;?>>Notizie</a>
+			<a "/views/prodotti.php?novita">Notizie</a>
 		    </li>
 		    <li>
-			<a <?=$prodotti_link;?>>Prodotti</a>
+			<a "/views/prodotti.php">Prodotti</a>
 		    </li>				
 		    <li>
-			<a <?=$servizi_link;?>>Servizi</a>
+			<a "/views/prodotti.php">Servizi</a>
 		    </li>
 		    <li>
-			<a <?=$contatti_link;?>>Contatti</a>
+			<a "/views/contatti.php">Contatti</a>
 		    </li>
 		</ul>
 	    </nav>
