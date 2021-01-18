7c318964996dc5e6c406d21f90797d6794acd7d0
Luca Zaninotto <lucazanny@gmail.com>
2021-01-18 22:42:32 +0100

    refactor importante

--- src/views/template/header.php
+++ src/views/template/header.php
@@ -1,12 +1,9 @@
 <?php
 require_once __DIR__ . DIRECTORY_SEPARATOR . '../../includes/resources.php';
 use function PRODOTTO\searchProdotti;
-use function VIEW\profileButton;
-use function VIEW\cartButton;
 use function UTILITIES\init_tag;
 
 init_tag($tag_home, '<a href="home.php">', $tag_home_close);
-init_tag($tag_info, '<a href="info.php">', $tag_info_close);
 init_tag($tag_novita, '<a href="prodotti.php?novita">', $tag_novita_close);
 init_tag($tag_prodotti, '<a href="prodotti.php">', $tag_prodotti_close);
 init_tag($tag_contatti, '<a href="contatti.php">', $tag_contatti_close);
@@ -49,12 +46,38 @@ echo '</form>
 		    </li>
 		    <li>
 			<!-- Test -->
-			' . profileButton() .'
-		    </li>
+			' . PHP_EOL;
+if(isset($_SESSION["username"])) {
+    // mostra icona utente
+    init_tag($tag_info, '<a href="info.php" id="user-icon">', $tag_cart_close);
+    echo $tag_info . "\uf007 " . $tag_info_close;
+}
+else {
+    echo '<a href="login.html">Entra</a>' . PHP_EOL;
+    echo '</li>' . PHP_EOL;
+    echo '<li>' . PHP_EOL;
+    echo '<a href="register.html">Registrati</a>' . PHP_EOL;
+}
+
+echo '</li>
 		    <li>
-			<!-- Test -->
-			' . cartButton() .'
-		    </li>
+			<!-- Test -->' . PHP_EOL;
+
+if(isset($_SESSION["cartID"])) {
+    // mostra icona cart con count degli item dentro
+    init_tag($tag_cart, '<a href="carrello.php" id="cart-icon">', $tag_cart_close);
+    $carrello = getProdottiFromCarrello($_SESSION["cartID"]);
+    $count = $carrello ? count($carrello) : 0;
+    echo $tag_cart . "\uf07a ". $count . $tag_cart_close;
+}
+else {
+    /* Non si dovrebbe mai arrivare qui, vorrebbe dire che si può
+     * accedere ad un punto senza cart inizializzato */
+    error_log("Qualcosa è andato storto.... debuggare forte");
+    die();
+}
+
+echo '</li>
 		</ul>
 	    </nav>
 	</header>';
