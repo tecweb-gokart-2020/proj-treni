9c12828647d5b7577b7eb718bf9bf418b4f264f5
Luca Zaninotto <lucazanny@gmail.com>
2021-01-18 22:19:02 +0100

    icona carrello insrita

--- src/views/functions/cartButton.php
+++ src/views/functions/cartButton.php
@@ -1,13 +1,18 @@
 <?php
-namespace VIEW;
 require_once __DIR__ . DIRECTORY_SEPARATOR . "../../includes/resources.php";
+use function CARRELLO\getProdottiFromCarrello;
 
-function cartButton() {
-    if(isset($_SESSION["cartID"])) {
-        // mostra icona cart con count degli item dentro
-    }
-    else {
-        // Mostra nulla...
-    }
+if(isset($_SESSION["cartID"])) {
+    // mostra icona cart con count degli item dentro
+    tag_init($tag_cart, '<a href="carrello.php" id="shopping-cart">', $tag_cart_close);
+    $carrello = getProdottiFromCarrello($_SESSION["cartID"]);
+    $count = $carrello ? count($carrello) : 0;
+    echo $tag_cart . "\uf07a ". $count . $tag_cart_close;
+}
+else {
+    /* Non si dovrebbe mai arrivare qui, vorrebbe dire che si può
+     * accedere ad un punto senza cart inizializzato */
+    error_log("Qualcosa è andato storto.... debuggare forte");
+    die();
 }
 ?>
