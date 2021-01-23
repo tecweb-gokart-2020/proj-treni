16f46b05b9c737559bcf97ddec6840dce20c18aa
badadumTss <lucazanny@gmail.com>
2021-01-23 12:32:57 +0000

    titolo in grande!!

--- src/views/template/header.php
+++ src/views/template/header.php
@@ -26,7 +26,7 @@ echo '<!DOCTYPE html>
         </nav>
 	<header>
 	    <!-- Logo -->
-	    <h1>'. $tag_home .'Trenene'. $tag_home_close .'</h1>
+	    <h1>'. $tag_home .'TRENENE'. $tag_home_close .'</h1>
 	    <!-- Ricerca -->
 	    <form id="ricercaHeader" action="prodotti.php" method="get">
 		<label for="searchQuery">Ricerca prodotti</label>
@@ -52,7 +52,7 @@ if(isset($_SESSION["username"])) {
     // mostra icona utente
     init_tag($tag_info, '<a href="info.php" id="user-icon">', $tag_info_close);
     echo $tag_info . '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="user" class="svg-inline--fa fa-user fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z"></path></svg>' . $tag_info_close;
-    echo '<li id="logout"><a href="logout.php">Logout</a></li>';
+    echo '</li><li id="logout"><a href="logout.php">Logout</a>';
 }
 else {
     echo '<a href="login.php">Entra</a>' . PHP_EOL;
