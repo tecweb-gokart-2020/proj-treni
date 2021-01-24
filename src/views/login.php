eda3b3112e2dbd554b81ac097ec3f7ebf1cd4b5e
BitStuffer <76245709+BitStuffer@users.noreply.github.com>
2021-01-24 12:48:24 +0100

    Revert "Merge branch 'Juvascrebt'"
    This reverts commit ceb6cce35d3b61e3d5374b098fabf87352039a2a, reversing
    changes made to 929dadedabbc6238adeb47f93f04978ae3fc8a83.

--- src/views/login.php
+++ src/views/login.php
@@ -5,17 +5,14 @@ session_start();
 echo '<main id="content">
 <div id="loginContainer" class="container">
 	<p><h1>Benvenuto!</h1>Accedi al tuo account</p>
-	<form id="formLogin" name="login" action="loginAct.php" method="post" novalidate="true" aria-live="assertive">
+	<form name="login" action="loginAct.php" method="post">
 		<label> <strong>Nome utente o Email</strong>
-			<input type="text" name="nomeUtente" id="loginUtente" maxlength="30"  autocomplete="on" aria-errormessage="errorUsername" aria-invalid="false" required />
+			<input type="text" name="nomeUtente" id="loginUtente" maxlength="30"  autocomplete="on" required />
 		</label>
-		<div id="errorUsername" class="errore" >Per favore inserisci nome utente o email</div>
 		<label> <strong>Password</strong>
-			<input type="password" name="password" id="loginPassword" maxlength="10" autocomplete="on" aria-errormessage="errorPassword" aria-invalid="false" required  />
+			<input type="password" name="password" id="loginPassword" maxlength="10" autocomplete="on" required  />
 		</label>
-		<div id="errorPassword" class="errore" >Per favore inserisci la password</div>
-		<label><input id="mostraPassword" type="checkbox">Mostra password</label>
-        <input type="submit" id="loginInvio" value="Accedi" >
+            <input type="submit" id="loginInvio" value="Accedi" >
 	</form>
 	<div id="loginToRegistr" class="container">
 		<p>Prima volta sul sito? <a href="register.php"><strong>Registrati!</strong></a></p>
@@ -27,8 +24,7 @@ if(isset($_SESSION["loginErr"])){
     unset($_SESSION["loginErr"]);
 }
 
-echo '</main>
-<script type="text/javascript" src="' . __DIR__ . DIRECTORY_SEPARATOR . '../styles/js/login.js"></script>';
+echo '</main>';
 include __DIR__ . DIRECTORY_SEPARATOR . 'template/footer.php';
 ?>
 
--- src/views/register.php
+++ src/views/register.php
@@ -16,24 +16,19 @@ echo '<main id="contenuto">
 		<div class="container">
 			<p><h1>Crea account</h1>Inserisci i dati per registrarti su trenene.it. I campi contrassegnati con * sono obbligatori.</p>
 
-			<form id="formRegistr" name="registr" action="registerAct.php" method="post"  novalidate="true" aria-live="assertive">
-				<label>Email *</br>
-						<input type="email" name="email" id="registrEmail" maxlength="100" autocomplete="on" aria-errormessage="errorEmail" aria-invalid="false" required />
-					</label>
-					<div id="errorEmail" class="errore" >Per favore inserisci un indirizzo email valido</div>
-					<label>Nome Utente *<div>Minimo 3 fra lettere e numeri, niente simboli o spazi</div>
-						<input type="text" name="nomeUtente" id="registrUtente" maxlength="20" aria-errormessage="errorUsername" aria-invalid="false" required/>
-					</label>
-					<div id="errorUsername" class="errore" >Per favore inserisci un nome utente valido</div>
-					<label>Password * <div>Massimo 10 caratteri e questi /\,.:;\'>< non sono consentiti</div>
-						<input type="password" name="password" id="registrPassword" maxlength="10" aria-errormessage="errorPassword" aria-invalid="false" required/>
-					</label>
-					<div id="errorPassword" class="errore" >Per favore inserisci una password valida</div>
-					<label>Conferma password *</br>
-						<input type="password" name="password2" id="registrPassword2" maxlength="10" aria-errormessage="errorPassword2" aria-invalid="false" required/>
-					</label>
-					<div id="errorPassword2" class="errore" >Le due password non coincidono</div>
-					<label><input id="mostraPassword" type="checkbox">Mostra password</label>
+			<form name="registr" action="registerAct.php" method="post">
+				<label>Email *
+					<input type="email" name="email" id="registrEmail" maxlength="100" autocomplete="on" required />
+				</label>
+				<label>Nome Utente * (Minimo 3 caratteri)<br/>
+					<input type="text" name="nomeUtente" id="registrUtente" maxlength="20" required/>
+				</label>
+				<label>Password * (Minimo 10 caratteri)<br/>
+					<input type="password" name="password" id="registrPassword" maxlength="10" required/>
+				</label>
+				<label>Conferma password *
+					<input type="password" name="password2" id="registrPassword2" maxlength="10" required/>
+				</label>
                     <input type="submit" value="Registrati"/>
 			</form>';
 
@@ -45,8 +40,7 @@ echo '<div id="RegistrToLogin" class="container">
 				<p>Già registrato? <a href="login.php"><strong>Accedi!</strong></a></p>
 			</div>	
 		</div>
-	</div></main>
-<script type="text/javascript" src="' . __DIR__ . DIRECTORY_SEPARATOR . '../styles/js/register.js"></script>';
+	</div></main>';
 
 include 'template/footer.php';
 ?>
